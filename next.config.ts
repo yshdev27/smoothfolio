f94d32e5c4c732bf153922707c7c1a400457f6eb
yshdev27 <gyash21@gmail.com>
2025-12-02 23:17:39 +0530

    fix https config

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,22 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "header",
+            key: "x-forwarded-proto",
+            value: "http",
+          },
+        ],
+        destination: "https://yashx.in/$1",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
